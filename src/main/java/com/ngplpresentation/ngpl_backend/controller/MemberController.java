aca72cc639510bd2d45b5ea9b76f3661bf82e872
hg2seo <hg2seo@naver.com>
2025-05-28 15:02:41 +0900

    테스트 간소화를 위한 HTTP 요청 매핑 수정

--- src/main/java/com/ngplpresentation/ngpl_backend/controller/MemberController.java
+++ src/main/java/com/ngplpresentation/ngpl_backend/controller/MemberController.java
@@ -6,10 +6,7 @@ import com.ngplpresentation.ngpl_backend.service.MemberService;
 import com.ngplpresentation.ngpl_backend.dto.response.ApiResponse;
 import com.ngplpresentation.ngpl_backend.dto.response.Status;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -18,7 +15,7 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/test")
+    @GetMapping("/test")
     public ApiResponse<?> test() {
         return ApiResponse.success(String.valueOf(Status.OK.getCode()),
             Status.OK.getMessage(), null);
